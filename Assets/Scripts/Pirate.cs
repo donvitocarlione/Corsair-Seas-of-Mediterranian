850281b12e3951091783095d7f6014a1712e761f
donvitocarlione <105321465+donvitocarlione@users.noreply.github.com>
2024-12-07 23:40:55 +0800

    Add base methods to Pirate class for overriding in Player

--- Assets/Scripts/Pirate.cs
+++ Assets/Scripts/Pirate.cs
@@ -1,46 +1,31 @@
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Pirate : MonoBehaviour
 {
-    public string pirateName;
-    public List<Ship> ships = new List<Ship>();
-    public Faction faction;
-    public Ship selectedShip;
+    protected List<Ship> ships = new List<Ship>();
+    protected FactionType faction;
 
-    public float combatSkill = 1f;
-    public float navigationSkill = 1f;
-    public float tradingSkill = 1f;
-
-    private void Start()
+    public virtual void SetFaction(FactionType newFaction)
     {
-        if (string.IsNullOrEmpty(pirateName))
+        faction = newFaction;
+        // Update faction for all ships
+        foreach (Ship ship in ships)
         {
-            GenerateRandomName();
+            if (ship != null)
+            {
+                ship.faction = newFaction;
+            }
         }
-    }
-
-    private void GenerateRandomName()
-    {
-        string[] titles = { "Captain", "Corsair", "Pirate", "Buccaneer" };
-        string[] firstNames = { "Black", "Red", "Mad", "Salty", "Iron", "Gold" };
-        string[] lastNames = { "Beard", "Hook", "Eye", "Dog", "Hand", "Heart" };
-        
-        pirateName = $"{titles[Random.Range(0, titles.Length)]} {firstNames[Random.Range(0, firstNames.Length)]} {lastNames[Random.Range(0, lastNames.Length)]}";
     }
 
     public virtual void AddShip(Ship ship)
     {
-        if (!ships.Contains(ship))
+        if (ship != null)
         {
             ships.Add(ship);
             ship.SetOwner(this);
-            ApplySkillsToShip(ship);
-            
-            if (ships.Count == 1)
-            {
-                SelectShip(ship);
-            }
+            ship.faction = faction;
         }
     }
 
@@ -49,56 +34,21 @@ public class Pirate : MonoBehaviour
         if (ships.Contains(ship))
         {
             ships.Remove(ship);
-            ship.ResetStats();
-            ship.SetOwner(null);
-            
-            if (selectedShip == ship)
-            {
-                selectedShip = ships.Count > 0 ? ships[0] : null;
-                if (selectedShip != null)
-                {
-                    SelectShip(selectedShip);
-                }
-            }
         }
     }
 
-    protected virtual void ApplySkillsToShip(Ship ship)
+    public virtual void SelectShip(Ship ship)
     {
-        if (ship != null)
-        {
-            if (ship.isCombatShip)
-            {
-                ship.currentAttack *= combatSkill;
-                ship.currentDefense *= combatSkill;
-            }
-
-            ShipMovement movement = ship.GetComponent<ShipMovement>();
-            if (movement != null)
-            {
-                movement.speedMultiplier *= navigationSkill;
-            }
-
-            ship.maxCargo *= (1f + (tradingSkill - 1f) * 0.5f);
-        }
+        // Base implementation does nothing
     }
 
-    public virtual bool SelectShip(Ship ship)
+    public virtual List<Ship> GetShips()
     {
-        if (ships.Contains(ship))
-        {
-            if (selectedShip != null)
-            {
-                selectedShip.Deselect();
-            }
-            selectedShip = ship;
-            return ship.Select();
-        }
-        return false;
+        return ships;
     }
 
-    public virtual void SetFaction(Faction newFaction)
+    public FactionType GetFaction()
     {
-        faction = newFaction;
+        return faction;
     }
 }
