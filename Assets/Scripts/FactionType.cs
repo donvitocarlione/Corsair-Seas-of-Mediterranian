7dbc3610183ab31188d5275fd4a5935fdaec580e
donvitocarlione <105321465+donvitocarlione@users.noreply.github.com>
2024-12-07 14:24:01 +0800

    Reorganize factions into historically accurate Mediterranean powers and update documentation

--- Assets/Scripts/FactionType.cs
+++ Assets/Scripts/FactionType.cs
@@ -1,22 +1,41 @@
+/// <summary>
+/// Represents all possible factions in the Mediterranean Sea
+/// </summary>
 public enum FactionType
 {
-    None,                  // No faction assigned
-    Player,                // Player's faction
+    None = 0,               // No faction assigned
+    Player,                 // Player's faction
     
-    // Historical Mediterranean Factions
-    BarbaryPirates,        // North African corsairs
-    MalteseCorsairs,       // Knights of St. John privateers
-    UscocPirates,          // Adriatic Sea pirates
-    BarbarianCorsairs,     // Independent Barbary corsairs
-    LevanticPirates,       // Eastern Mediterranean pirates
-    GreekPirates,          // Aegean Sea pirates
-    CilicianPirates,       // Ancient Mediterranean pirates
-    ProvencalPirates,      // French Mediterranean pirates
-    AlgerianTaifa,         // Algerian privateers
-    RenegadoPirates,       // European converts to Islam who became pirates
+    // Major Powers
+    OttomanEmpire,          // Ottoman Turkish Empire
+    VenetianRepublic,       // Republic of Venice
+    SpanishEmpire,          // Spanish Habsburg Empire
+    KnightsOfMalta,         // Knights Hospitaller/Order of St. John
+    PapalStates,            // The Catholic Church's territories
     
-    // Generic Factions
-    Pirates,               // Generic pirates
-    Merchants,             // Trading vessels
-    Navy                   // Naval forces
+    // North African Powers
+    AlgiersCorsairs,        // Barbary Corsairs of Algiers
+    TunisCorsairs,          // Barbary Corsairs of Tunis
+    TripoliCorsairs,        // Barbary Corsairs of Tripoli
+    MoroccanCorsairs,       // Corsairs of the Moroccan Sultanate
+    
+    // Mediterranean Pirates
+    UscocPirates,           // Adriatic Sea pirates
+    GreekPirates,           // Aegean Sea pirates
+    LevanticPirates,        // Eastern Mediterranean pirates
+    BarbarianPirates,       // Independent Barbary pirates
+    RenegadoPirates,        // European converts who became pirates
+    
+    // Merchant Factions
+    VenetianMerchants,      // Venetian trading fleet
+    GenoeseMerchants,      // Genoese trading network
+    RagusanMerchants,      // Dubrovnik/Ragusa merchants
+    GreekMerchants,        // Greek trading vessels
+    JewishMerchants,       // Jewish trading networks
+    
+    // Generic Types
+    Pirates,                // Generic pirate faction
+    Merchants,              // Generic merchant faction
+    Navy,                   // Generic naval forces
+    Neutral                 // Neutral vessels
 }
