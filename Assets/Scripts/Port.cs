51c5f7f82dcfdbe217081161e28af762d3c3e1b8
donvitocarlione <105321465+donvitocarlione@users.noreply.github.com>
2024-12-08 12:49:30 +0800

    Update Port.cs: Implement proper faction management and ownership

--- Assets/Scripts/Port.cs
+++ Assets/Scripts/Port.cs
@@ -1,12 +1,59 @@
 using UnityEngine;
+using CSM.Base;
 
-public class Port : MonoBehaviour
+public class Port : SeaEntityBase
 {
-    public string portName;
-    public Faction controllingFaction;
+    [SerializeField]
+    private FactionType owningFaction = FactionType.None;
     
-    public void SetControllingFaction(Faction faction)
+    [SerializeField]
+    private string portName;
+    
+    public FactionType OwningFaction => owningFaction;
+    public string PortName => portName;
+
+    public void SetFaction(FactionType newFaction)
     {
-        controllingFaction = faction;
+        if (owningFaction != newFaction)
+        {
+            var oldFaction = owningFaction;
+            owningFaction = newFaction;
+            
+            // Notify FactionManager of the change
+            if (FactionManager.Instance != null)
+            {
+                FactionManager.Instance.HandlePortCapture(newFaction, this);
+            }
+            
+            Debug.Log($"Port {portName} changed ownership from {oldFaction} to {newFaction}");
+        }
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        
+        // Register with initial faction
+        if (owningFaction != FactionType.None && FactionManager.Instance != null)
+        {
+            var factionData = FactionManager.Instance.GetFactionData(owningFaction);
+            if (factionData != null)
+            {
+                factionData.AddPort(this);
+            }
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        // Unregister from current faction
+        if (owningFaction != FactionType.None && FactionManager.Instance != null)
+        {
+            var factionData = FactionManager.Instance.GetFactionData(owningFaction);
+            if (factionData != null)
+            {
+                factionData.RemovePort(this);
+            }
+        }
     }
 }
